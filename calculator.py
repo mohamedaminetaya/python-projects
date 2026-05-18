80b04f64c557e3c3330e9c98ca55267324755c6b
mohamedaminetaya <mohamed.aminetaya.pro@gmail.com>
2026-05-18 21:40:15 +0100

    Create calculator.py
    Add Calculator project

---
+++ calculator.py
@@ -0,0 +1,36 @@
+try:
+
+    print("=== Calculator  ===")
+
+    print("1 Addition")
+    print("2 Subtraction")
+    print("3 Multiplication")
+    print("4 Division")
+
+    choice = int(input("Choose operation : "))
+
+    n1 = int(input("Enter first number : "))
+    n2 = int(input("Enter second number : "))
+
+    if choice == 1:
+        print("Result =", n1 + n2)
+
+    elif choice == 2:
+        print("Result =", n1 - n2)
+
+    elif choice == 3:
+        print("Result =", n1 * n2)
+
+    elif choice == 4:
+
+        if n2 == 0:
+            print("Impossible division by zero")
+
+        else:
+            print("Result =", n1 / n2)
+
+    else:
+        print("Invalid choice")
+
+except:
+    print("Error : enter valid numbers")
